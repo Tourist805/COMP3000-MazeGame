4d7baed1422c086905cb079164a5960a5264d07e
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-03-12 21:03:07 +0000

    Continued working with AI
    Improved animations for the other enemy type

---
+++ Assets/FPSModels/Scripts/PlayerScripts/AttackDetecting.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackDetecting : MonoBehaviour
+{
+    [SerializeField] private float _damage = 2f;
+    [SerializeField] private float _radius = 1f;
+    [SerializeField] private LayerMask _layerMask;
+
+    private void Update()
+    {
+        ObserveHits();
+    }
+
+    private void ObserveHits()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, _radius, _layerMask);
+
+        if(hits.Length > 0)
+        {
+            //hits[0].gameObject.TryGetComponent()
+            
+            Debug.Log("Touched " + hits[0].gameObject.tag);
+            gameObject.SetActive(false);
+        }
+    }
+}
