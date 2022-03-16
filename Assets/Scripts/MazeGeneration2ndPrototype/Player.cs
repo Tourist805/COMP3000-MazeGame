2371b56da2430d08839e26cf63093fbd23790777
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-03-16 22:08:38 +0000

    Refactored and playtested the maze prefabs

--- Assets/Scripts/MazeGeneration2ndPrototype/Player.cs
+++ Assets/Scripts/MazeGeneration2ndPrototype/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
 	private MazeCell currentCell;
 	private MazeDirection _currentDirection;
+	[SerializeField]private bool _isInitialPlayer = false;
 	private void Look(MazeDirection direction)
     {
 		transform.localRotation = direction.ToRotation();
@@ -34,29 +35,34 @@ public class Player : MonoBehaviour
 
 	private void Update()
 	{
-		//if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-		//{
-		//	Move(_currentDirection);
-		//}
-		//else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-		//{
-		//	Move(_currentDirection.GetNextClockwise());
-		//}
-		//else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-		//{
-		//	Move(_currentDirection.GetOpposite());
-		//}
-		//else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-		//{
-		//	Move(_currentDirection.GetNextCounterclockwise());
-		//}
-		//else if (Input.GetKeyDown(KeyCode.Q))
-		//{
-		//	Look(_currentDirection.GetNextCounterclockwise());
-		//}
-		//else if (Input.GetKeyDown(KeyCode.E))
-		//{
-		//	Look(_currentDirection.GetNextClockwise());
-		//}
+		if (_isInitialPlayer)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Move(_currentDirection);
+            }
+            else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Move(_currentDirection.GetNextClockwise());
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                Move(_currentDirection.GetOpposite());
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                Move(_currentDirection.GetNextCounterclockwise());
+            }
+            else if (Input.GetKeyDown(KeyCode.Q))
+            {
+                Look(_currentDirection.GetNextCounterclockwise());
+            }
+            else if (Input.GetKeyDown(KeyCode.E))
+            {
+                Look(_currentDirection.GetNextClockwise());
+            }
+        }
+
+
 	}
 }
