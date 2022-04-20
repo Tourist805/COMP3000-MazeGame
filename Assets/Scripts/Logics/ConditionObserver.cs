8bd05876e69027ece826d1de34bacecd76774413
Zhanybek <57710352+Tourist805@users.noreply.github.com>
2022-04-20 16:29:03 +0100

    Added Winning condition

--- Assets/Scripts/Logics/ConditionObserver.cs
+++ Assets/Scripts/Logics/ConditionObserver.cs
@@ -5,7 +5,27 @@ using UnityEngine;
 public class ConditionObserver : MonoBehaviour
 {
     [SerializeField] private GameOverUI _gameoverUI;
+    [SerializeField] private int _winingCondition = 40;
+    public int WiningCondition
+    {
+        get
+        {
+            return _winingCondition;
+        }
+        set
+        {
+            _winingCondition = value;
+        }
+    }
 
+    private void Update()
+    {
+        if(Coin.Count >= _winingCondition)
+        {
+            _gameoverUI.SetCondition(true);
+            _gameoverUI.Open();
+        }
+    }
     private void OnEnable()
     {
         PlayerHealth.PlayerDead += _gameoverUI.Open;
